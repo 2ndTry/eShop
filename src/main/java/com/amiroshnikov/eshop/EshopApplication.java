6a59a6e4119dcb62d6d06916b0e26c3273e4736c
2ndTry <mirnijalex@gmail.com>
2022-01-05 14:45:58 +0300

    Add SOAP

--- src/main/java/com/amiroshnikov/eshop/EshopApplication.java
+++ src/main/java/com/amiroshnikov/eshop/EshopApplication.java
@@ -8,12 +8,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class EshopApplication {
 
-
 	public static void main(String[] args) {
 
 		ConfigurableApplicationContext context = SpringApplication.run(EshopApplication.class, args);
 		PasswordEncoder encoder = context.getBean(PasswordEncoder.class);
 		System.out.println(encoder.encode("pass"));
 	}
-
 }
